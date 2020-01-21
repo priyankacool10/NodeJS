9260e325b7ddad805c995563ea8242645e13a154
priyankakapoor <priyanka.kapoor@tungsten-network.com>
2020-01-21 16:29:52 +0530

    Http Get Request Program

---
+++ Programs/HttpModuleGet.js
@@ -0,0 +1,24 @@
+//Importing http module to make Get HTTP request to Weather API
+const http = require('http');
+//Getting City name from commandline argument. Default is set to Delhi.
+let city= process.argv[2] ||"Delhi";
+
+/**Tweeked given URL to include query string parameter units=metric for Temperature in Celsius.
+** Also made city as variable in URL **/
+http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2b421cd305b88e39d4182ac89b85e186`, (response) => {
+let data = '';
+
+  // Appending parts of data received.
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // On getting whole response, printing the temperature of given City on Console.
+  response.on('end', () => {
+    let weather = JSON.parse(data);
+    console.log(`It's ${weather.main.temp} degree in ${city} `);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
---
+++ Programs/JsonRead.js
@@ -0,0 +1,1 @@
+const url='https://github.com/priyankacool10/NodeJS/blob/master/JSON/JsonHolidays.js';
