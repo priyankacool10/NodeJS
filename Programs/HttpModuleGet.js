233e2878933937eeed827ba7c5c9a2c126a53db5
priyankakapoor <priyanka.kapoor@tungsten-network.com>
2020-01-21 16:33:55 +0530

    added url in a variable"

--- Programs/HttpModuleGet.js
+++ Programs/HttpModuleGet.js
@@ -1,11 +1,14 @@
 //Importing http module to make Get HTTP request to Weather API
 const http = require('http');
+
 //Getting City name from commandline argument. Default is set to Delhi.
 let city= process.argv[2] ||"Delhi";
 
+const url=`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2b421cd305b88e39d4182ac89b85e186`;
+
 /**Tweeked given URL to include query string parameter units=metric for Temperature in Celsius.
 ** Also made city as variable in URL **/
-http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2b421cd305b88e39d4182ac89b85e186`, (response) => {
+http.get(url, (response) => {
 let data = '';
 
   // Appending parts of data received.
