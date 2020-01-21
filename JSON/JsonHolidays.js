b1ea4012b2fab1984ecb3d1b046f95b1a8ecf938
priyankakapoor <priyanka.kapoor@tungsten-network.com>
2020-01-21 14:26:21 +0530

    JSON File for API Call

---
+++ JSON/JsonHolidays.js
@@ -0,0 +1,34 @@
+{
+	"publicHolidays": [{
+			"title": "New Year Day",
+			"date": "2020-01-01"
+		},
+		{
+			"title": "Republic Day",
+			"date": "2020-01-26"
+		},
+		{
+			"title": "Independence Day",
+			"date": "2020-08-15"
+		},
+		{
+			"title": "Gandhi Jayanti",
+			"date": "2020-10-02"
+		}
+	],
+	"flexibleHolidays": [
+
+		{
+			"title": "Makar Sankranti",
+			"date": "2020-01-15"
+		},
+		{
+			"title": "Basant Panchmi",
+			"date": "2020-02-09"
+		},
+		{
+			"title": "Election day",
+			"date": "2020-02-08"
+		}
+	]
+}
