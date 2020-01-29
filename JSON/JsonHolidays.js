1c29054a7c240cc742cd0f213bb0420b47e2a454
priyankakapoor <priyanka.kapoor@tungsten-network.com>
2020-01-29 11:50:36 +0530

    JSON file updated

--- JSON/JsonHolidays.js
+++ JSON/JsonHolidays.js
@@ -1,34 +1,55 @@
 {
-	"publicHolidays": [{
-			"title": "New Year Day",
-			"date": "2020-01-01"
-		},
-		{
-			"title": "Republic Day",
-			"date": "2020-01-26"
-		},
-		{
-			"title": "Independence Day",
-			"date": "2020-08-15"
-		},
-		{
-			"title": "Gandhi Jayanti",
-			"date": "2020-10-02"
-		}
-	],
-	"flexibleHolidays": [
+	"publicHolidays": {
 
-		{
-			"title": "Makar Sankranti",
-			"date": "2020-01-15"
-		},
-		{
-			"title": "Basant Panchmi",
-			"date": "2020-02-09"
-		},
-		{
-			"title": "Election day",
-			"date": "2020-02-08"
-		}
-	]
+		"january": [{
+				"title": "New Year Day",
+				"date": "2020-01-01"
+			},
+			{
+				"title": "Republic Day",
+				"date": "2020-01-26"
+			},
+			{
+				"title": "Gandhi Jayanti",
+				"date": "2020-10-02"
+			}
+		],
+		"august": [{
+				"title": "Independence Day",
+				"date": "2020-08-15"
+			},
+			{
+				"title": "Raksha Bandhan",
+				"date": "2020-08-26"
+			}
+		],
+		"october": [{
+				"title": "Gandhi Jayanti",
+				"date": "2020-10-02"
+			},
+			{
+				"title": "Diwali",
+				"date": "2020-10-30"
+			}
+
+		]
+	},
+	"flexibleHolidays": {
+		"january": [
+
+			{
+				"title": "Makar Sankranti",
+				"date": "2020-01-15"
+			}
+		],
+		"february": [{
+				"title": "Basant Panchmi",
+				"date": "2020-02-09"
+			},
+			{
+				"title": "Election day",
+				"date": "2020-02-08"
+			}
+		]
+	}
 }
