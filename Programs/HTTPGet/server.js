7ff39ae2f3c73135bd9c96d45114c56a33ff6f35
priyankakapoor <priyanka.kapoor@tungsten-network.com>
2020-01-29 11:51:14 +0530

    Routing in NodeJS

--- Programs/HTTPGet/server.js
+++ Programs/HTTPGet/server.js
@@ -5,49 +5,60 @@ const jsonUrl='https://raw.githubusercontent.com/priyankacool10/NodeJS/master/JS
 var gitResponse;
 // Create a server object 
 http.createServer(function (request, response) { 
-      
+      var dataR;
     // http responseheader 
-    response.writeHead(200, {'Content-Type': 'application/json'});  
+    response.writeHead(200);//, {'Content-Type': 'application/json'});  
       
-    var requestPath = request.url; 
+    var requestPath = url.parse(request.url).pathname; 
     var queryData = url.parse(request.url, true).query;
     console.log("Query String: "+ queryData.month);
     response.write("Query String: "+ queryData.month);
-        if(requestPath =='/public') { 
-        response.write(`Public holidays of month ${queryData.month}`); 
+    response.write("URL Path Name: "+ requestPath);
+    var body = '';
+   
+    switch(requestPath){
+        case "/":
+            response.write("Welcome To Home Page!");
+            break;
+        case "/public":
+            
+        //response.write(`Public holidays of month ${queryData.month}`); 
         
         https.get(jsonUrl,(res)=>{
-            var body = '';
+            
 
             res.on('data', function(chunk){
                 body += chunk;
+                gitResponse = JSON.parse(body).publicHolidays;
+                response.write('JSON Data'+JSON.stringify(gitResponse));
+                console.log("Got a response: ", gitResponse);
             });
         
-            res.on('end', function(){
-                gitResponse = JSON.parse(body).publicHolidays;
-                console.log("Got a response: ", gitResponse);
-                gitResponse= JSON.stringify(body).publicHolidays;
+            res.on('end', function()
+            {
+               // res.json(body);
+                response.end();
+               // res.pipe(response);
             });
         }).on("error", (err) => {
             console.log("Error: " + err.message);
           }); 
-        response.write("List of all Public Holidays:" + gitResponse);
-       // response.JSON(gitResponse);
-        response.end();  
-    } 
-    else if(requestPath ==='/flexible/month') { 
-        response.write('Flexible Holidays');  
-        response.end();  
-    } 
-    else { 
-        response.write('Holiday not found');  
-        response.end();  
-    } 
+                break;
+        case "/flexible":
+            response.write(`Public holidays of month ${queryData.month}`); 
+            break;
+        default:
+            response.write('Holiday not found');  
+            response.end();      
+            break;
+
+    }
 }).listen(3001,()=>{
  // The server object listens on port 3001
     console.log("server start at port 3001"); 
 
 });
+
       
    
  
